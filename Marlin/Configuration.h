6e9461e3853a383dedbafed2b93d90b515e5ee22
Jaykkin <51738128+Jaykkin@users.noreply.github.com>
2021-05-19 11:17:35 -0500

    Updated PID Tune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.21
-    #define DEFAULT_Ki   1.57
-    #define DEFAULT_Kd  78.67
+    #define DEFAULT_Kp  32.91
+    #define DEFAULT_Ki   2.91
+    #define DEFAULT_Kd  93.14
   #endif
 #endif // PIDTEMP
 
